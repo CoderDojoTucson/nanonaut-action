0500582390d6b11bb5d66a8237d581cfb6a072be
Dan Stormont <daniel.stormont@gmail.com>
2021-07-20 22:47:24 +0000

    Level up! Now you know how to create shapes and images in a program.

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
-var name = 'Nanonauts!';
-var message = ("I can't believe how good they are.");
-console.log(name + ' ' + message);
+var canvas = document.createElement('canvas');
+canvas.width = 800;
+canvas.height = 600;
+document.body.appendChild(canvas);
+
+var c = canvas.getContext('2d');
+
+var image = new Image();
+image.src = 'images/Nanonaut.png';
+
+function start() {
+  c.fillStyle = 'green';
+  c.fillRect(10, 10, 30, 30);
+  c.drawImage(image, 20, 40);
+}
+
+window.addEventListener('load', start);
