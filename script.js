d9bad233c07b484ad61e35745d6b0fbc49a90bad
Dan Stormont <daniel.stormont@gmail.com>
2021-07-21 23:25:41 +0000

    Level up! We see a Nanonaut in an exciting world.

--- script.js
+++ script.js
@@ -1,26 +1,64 @@
+// CONSTANTS
+var CANVAS_WIDTH = 800;
+var CANVAS_HEIGHT = 600;
+var NANONAUT_WIDTH = 181;
+var NANONAUT_HEIGHT = 229;
+var NANONAUT_START_X = 50;
+var NANONAUT_START_Y = 40;
+var GROUND_Y = 540;
+var BACKGROUND_Y = -200;
+
+// SETUP
 var canvas = document.createElement('canvas');
-canvas.width = 800;
-canvas.height = 600;
+var c = canvas.getContext('2d');
+canvas.width = CANVAS_WIDTH;
+canvas.height = CANVAS_HEIGHT;
 document.body.appendChild(canvas);
 
-var c = canvas.getContext('2d');
+var nanonautImage = new Image();
+nanonautImage.src = 'images/Nanonaut.png';
 
-var image = new Image();
-image.src = 'images/Nanonaut.png';
+// Define background image.
+var backgroundImage = new Image();
+backgroundImage.src = 'images/background.png';
 
-function start() {
-  window.requestAnimationFrame(loop);
-}
-
-var x = 0;
-var y = 40;
-
-function loop() {
-  c.clearRect(0, 0, 800, 600);
-  c.drawImage(image, x, y);
-  x = x + 1;
-
-  window.requestAnimationFrame(loop);
-}
+var nanonautX = NANONAUT_START_X;
+var nanonautY = NANONAUT_START_Y;
 
 window.addEventListener('load', start);
+
+function start() {
+  window.requestAnimationFrame(mainLoop);
+}
+
+// MAIN LOOP
+function mainLoop() {
+  update();
+  draw();
+  window.requestAnimationFrame(mainLoop);
+}
+
+// PLAYER INPUT
+
+// UPDATING
+function update() {
+
+}
+
+// DRAWING
+function draw() {
+  // Draw the sky.
+  c.fillStyle = 'LightSkyBlue';
+  c.fillRect(0, 0, CANVAS_WIDTH, GROUND_Y - 40);
+
+  // Draw the background.
+  c.drawImage(backgroundImage, 0, BACKGROUND_Y);
+
+  // Draw the ground.
+  c.fillStyle = 'ForestGreen';
+  c.fillRect(0, GROUND_Y - 40, 
+             CANVAS_WIDTH, CANVAS_HEIGHT - GROUND_Y + 40);
+
+  // Draw the Nanonaut.
+  c.drawImage(nanonautImage, nanonautX, nanonautY);
+}
